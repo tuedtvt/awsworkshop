6250516843ca2086b0b699911a7f36fe25739dc2
TueNT <TueNT@Tues-MacBook-Pro.local>
2021-11-07 18:53:28 +0700

    v2

--- module-2/app/service/mythicalMysfitsService.py
+++ module-2/app/service/mythicalMysfitsService.py
@@ -40,4 +40,4 @@ def getMysfits():
 # Run the service on the local server it has been deployed to,
 # listening on port 8080.
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=8088)
